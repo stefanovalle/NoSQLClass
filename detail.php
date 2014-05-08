2fa596b98067aadc7f552a5d89b30fccdab3ddff
Steve <steve@maraspin.net>
2014-05-08 03:20:08 +0200

    First app ver

--- detail.php
+++ detail.php
@@ -1,3 +1,49 @@
 <?php
 
-echo "Hello NoSQL";
+
+ini_set('display_errors', 1);
+$dsn = 'pgsql:host=localhost;dbname=ecommerce';
+
+
+$id = $_GET['id'];
+
+try {
+
+  $db = new PDO($dsn , 'postgres', 'zf2');
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+  $sql = "SELECT prodotto.*, macrocategoria.nome as macrocategoria, variante.nome as variante,
+          categoria.nome as categoria FROM prodotto join categoria on categoria.id = prodotto.categoria_id 
+          join macrocategoria on macrocategoria.id = categoria.macrocategoria_id 
+          join prodottovariante on prodotto.id = prodottovariante.id_prodotto 
+          join variante on prodottovariante.id_variante = variante.id
+          WHERE prodotto.id = ". $id ;
+
+  $start = microtime(true);
+  
+  $st = $db->query($sql);
+  $row = $st->fetch();
+  
+}
+  catch (PDOException $e) {
+    print $e->getMessage();
+}
+?>
+<h1>Scheda Prodotto: <?php echo $row['nome']; ?></h1>
+<p>Prezzo: <?php echo $row['prezzo']; ?></p>
+<p>Venduti: <?php echo $row['venduti']; ?></p>
+<p>Disp. Dal: <?php echo $row['dataarrivo']; ?></p>
+<p>Varianti: <?php echo $row['variante']; ?>
+    
+<?php  
+    while ($row = $st->fetch()) {
+          echo ', '.$row['variante'];
+        }
+    $time_taken = microtime(true) - $start;
+?>
+</p>
+
+<?php echo "Time taken: " . $time_taken; ?>
+
+<br />
+<a href="/index.php">Back</a>
--- index.php
+++ index.php
@@ -1,3 +1,54 @@
+<h1>NoSQL E-Commerce</h1>
+
+<h2>Tutti gli articoli...</h2>
+<table>
+    <thead>
+        <td>Macrocategoria</td>
+        <td>Categoria</td>
+        <td>Prodotto</td>
+        <td>Prezzo</td>
+        <td>Acquisti</td>
+        <td>Data Arrivo</td>
+        <td>Dettaglio</td>
+    </thead>
 <?php
 
-echo "Hello NoSQL";
+ini_set('display_errors', 1);
+$dsn = 'pgsql:host=localhost;dbname=ecommerce';
+
+try {
+
+  $db = new PDO($dsn , 'postgres', 'zf2');
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+  $sql = 'SELECT prodotto.*, macrocategoria.nome as macrocategoria, 
+          categoria.nome as categoria FROM prodotto join categoria on categoria.id = prodotto.categoria_id 
+          join macrocategoria on macrocategoria.id = categoria.macrocategoria_id 
+          ORDER by prodotto.dataarrivo DESC, categoria.nome, prodotto.nome LIMIT 10';
+
+  $start = microtime(true);
+
+  foreach($db->query($sql) as $row){  
+      ?>
+    <tr>
+    <td><?php echo $row['macrocategoria']; ?></td>
+        <td><?php echo $row['categoria']; ?></td>
+        <td><?php echo $row['nome']; ?></td>
+        <td><?php echo $row['prezzo']; ?></td>
+        <td><?php echo $row['venduti']; ?></td>
+        <td><?php echo $row['dataarrivo']; ?></td>
+        <td><a href="/detail.php?id=<?php echo $row['id']; ?>">Vedi</a></td>
+    </tr>
+      <?php
+  }
+  
+  $time_taken = microtime(true) - $start;
+  
+}
+  catch (PDOException $e) {
+    print $e->getMessage();
+}
+?>
+</table>
+
+<?php echo "Time taken: " . $time_taken; ?>
