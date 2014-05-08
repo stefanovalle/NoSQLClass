d2eb9682b4107ac12494fc90eba7a5e7d0aa3dcc
Steve <steve@maraspin.net>
2014-05-08 03:44:04 +0200

    Memcached added

--- detail.php
+++ detail.php
@@ -12,35 +12,49 @@ try {
   $db = new PDO($dsn , 'postgres', 'zf2');
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   
-  $sql = "SELECT prodotto.*, macrocategoria.nome as macrocategoria, variante.nome as variante,
-          categoria.nome as categoria FROM prodotto join categoria on categoria.id = prodotto.categoria_id 
-          join macrocategoria on macrocategoria.id = categoria.macrocategoria_id 
-          join prodottovariante on prodotto.id = prodottovariante.id_prodotto 
-          join variante on prodottovariante.id_variante = variante.id
-          WHERE prodotto.id = ". $id ;
+  $memcache = new \Memcached();
+  $memcache->addServer('localhost', 11211) or die ("Could not connect");
+  
+   $start = microtime(true);
+  
+  $item = $memcache->get($id);
+  
+  if (!$item) {
+  
+    $sql = "SELECT prodotto.*, macrocategoria.nome as macrocategoria, variante.nome as variante,
+            categoria.nome as categoria FROM prodotto join categoria on categoria.id = prodotto.categoria_id 
+            join macrocategoria on macrocategoria.id = categoria.macrocategoria_id 
+            join prodottovariante on prodotto.id = prodottovariante.id_prodotto 
+            join variante on prodottovariante.id_variante = variante.id
+            WHERE prodotto.id = ". $id ;
 
-  $start = microtime(true);
-  
-  $st = $db->query($sql);
-  $row = $st->fetch();
-  
+    $st = $db->query($sql);
+    $row = $st->fetch();
+    $item = $row;
+
+    $varianti = $row['variante'];
+    while ($row = $st->fetch()) {
+            $varianti .= ', ' . $row['variante'];
+    }
+    
+    $item['variante'] = $varianti;
+    
+    $memcache->set($id, $item, 10) or die ("Failed to save data at the server");
+
+  }
+
+  $time_taken = microtime(true) - $start;  
+          
 }
   catch (PDOException $e) {
     print $e->getMessage();
 }
 ?>
-<h1>Scheda Prodotto: <?php echo $row['nome']; ?></h1>
-<p>Prezzo: <?php echo $row['prezzo']; ?></p>
-<p>Venduti: <?php echo $row['venduti']; ?></p>
-<p>Disp. Dal: <?php echo $row['dataarrivo']; ?></p>
-<p>Varianti: <?php echo $row['variante']; ?>
-    
-<?php  
-    while ($row = $st->fetch()) {
-          echo ', '.$row['variante'];
-        }
-    $time_taken = microtime(true) - $start;
-?>
+<h1>Scheda Prodotto: <?php echo $item['nome']; ?></h1>
+<p>Prezzo: <?php echo $item['prezzo']; ?></p>
+<p>Venduti: <?php echo $item['venduti']; ?></p>
+<p>Disp. Dal: <?php echo $item['dataarrivo']; ?></p>
+<p>Varianti: <?php echo $item['variante']; ?>
 </p>
 
 <?php echo "Time taken: " . $time_taken; ?>
